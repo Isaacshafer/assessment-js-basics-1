071743e44787b7a78edd36cdfd2b6fbd7c9091bf
Isaac Shafer <izakshafer@gmail.com>
2022-02-11 10:50:25 -0700

    started password

---
+++ password.js
@@ -0,0 +1,10 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+reader.question("What would you like to calculate?", function(input){
+	tokens = input.split(' ');
