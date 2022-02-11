ece2d81cf81ac6f4bfbc38ef7cf9ca76c5d2c9a2
Isaac Shafer <izakshafer@gmail.com>
2022-02-11 11:37:15 -0700

    finished password

--- password.js
+++ password.js
@@ -6,5 +6,25 @@ const reader = readline.createInterface({
 });
 
 
-reader.question("What would you like to calculate?", function(input){
+
+reader.question("welcome to the password validator tool, what password would you like to validate?", function(input){
 	tokens = input.split(' ');
+	
+	password = tokens[0];
+	
+    if (password.length < 10){
+        console.log('you idiot, you need a longer password')
+    }else if (password.length > 150){
+        console.log('( * ) ( * )')
+    }
+    else{
+        console.log("looks great bro")
+    }
+
+	reader.close()
+
+
+});
+
+
+
